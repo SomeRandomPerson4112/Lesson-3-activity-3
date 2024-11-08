55f11d74cf5f1fbc4b93eaaae90e470cfb191ed9
SomeRandomPerson4112 <ayaangadani@gmail.com>
2024-11-08 19:59:45 +0500

    main.12345

---
+++ String operation.py
@@ -0,0 +1,8 @@
+text=str(input("enter a string: "))
+reverse_text=text[::-1]
+print("Reverse of the given string is: ")
+print(reverse_text)
+str="hello"
+print(str[1:4])
+print(str[1::2])
+print(reverse_text+str)
